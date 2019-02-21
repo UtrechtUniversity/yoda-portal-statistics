7becf11861e97be52131f61c1cdeb3dd3d2e6819
Lazlo Westerhof <l.r.westerhof@uu.nl>
2019-02-21 12:34:36 +0100

    Add documentation.

--- helpers/bytes_helper.php
+++ helpers/bytes_helper.php
@@ -1,13 +1,40 @@
 <?php
+/**
+* Bytes helper
+ *
+ * @package    Yoda
+ * @copyright  Copyright (c) 2017-2019, Utrecht University. All rights reserved.
+ * @license    GPLv3, see LICENSE.
+ */
+
+ /**
+  * Convert bytes to terabytes.
+  *
+  * @param $bytes Number of bytes
+  * @return float Number of terabytes
+  */
 function bytesToTerabytes($bytes) {
     //https://www.checkyourmath.com/convert/digital_storage/bytes_terabytes.php
     return $bytes / 1000000000000;
 }
 
+/**
+ * Convert bytes to gigabytes.
+ *
+ * @param $bytes Number of bytes
+ * @return float Number of gigabytes
+ */
 function bytesToGigabytes($bytes) {
     return $bytes / 1000000000;
 }
 
+/**
+ * Round up bytes.
+ *
+ * @param value  Number of bytes
+ * @param places Decimal places to round up to
+ * @return float Number of bytes
+ */
 function roundUpBytes ($value, $places=0) {
     $mult = pow(10, $places);
     return ceil($value * $mult) / $mult;
